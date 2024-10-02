2bd0db44562480407b85840b810972830b6b791d
muzi5@kakao.com <muzi5@kakao.com>
2024-10-02 20:35:23 +0900

    문제 발생
    초기 값이 없어서 loading 엘리먼트가 화면에 나와가지고 기존값 1 에서 바로 +1 하는 로직이 실행됨

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import type { MockData, IGetProducts } from "./type";
 import axios from "axios";
 import Item from "./components/Item";
 
 function App() {
+  const ref = useRef<HTMLLIElement>(null);
   const [currentPage, setCurrentPage] = useState<number>(0);
   const [data, setData] = useState<MockData[]>([]);
 
   const getData = async () => {
     const { data }: IGetProducts = await axios.get("/data.json");
-    setData((prev) => [...prev, ...data.products]);
+    console.log(currentPage * 10, (currentPage + 1) * 10);
+    const sliceData = data.products.slice(currentPage * 10, (currentPage + 1) * 10);
+    setData((prev) => [...prev, ...sliceData]);
     return data;
   };
 
   useEffect(() => {
     getData();
+  }, [currentPage]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(async (entry) => {
+          if (entry.isIntersecting) {
+            setCurrentPage((prev) => prev + 1);
+          }
+        });
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
   }, []);
   return (
     <>
@@ -25,6 +49,8 @@ function App() {
         {data.map((el, idx) => {
           return <Item key={el.productId} index={idx} {...el} />;
         })}
+
+        <li ref={ref}>Loading ...</li>
       </ul>
     </>
   );
